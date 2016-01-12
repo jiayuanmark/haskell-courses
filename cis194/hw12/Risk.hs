28b3da3ec3dc37425da7c78eb7331f022c4fbb58
jiayuanmark <jiayuanmark@gmail.com>
2016-01-12 00:26:34 -0800

    fix hw11 bug and hw12

--- cis194/hw12/Risk.hs
+++ cis194/hw12/Risk.hs
@@ -4,6 +4,7 @@ module Risk where
 
 import Control.Monad
 import Control.Monad.Random
+import Data.Array
 import Data.List (sortBy)
 
 ------------------------------------------------------------
@@ -29,16 +30,47 @@ type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
 
+play :: [DieValue] -> [DieValue] -> (Int, Int)
+play attackDie defendDie = (length zp - n, n)
+  where f  = sortBy (flip compare)
+        zp = zipWith (compare) (f attackDie) (f defendDie)
+        n  = length . filter (== GT) $ zp
+
 battle :: Battlefield -> Rand StdGen Battlefield
 battle (Battlefield a d) = do
-  ad <- replicateM na die
-  sd <- replicateM nb die
-  let (da, dd) = play ad sd
-  return (Battlefield (a-da) (d-dd))
-  where na       = min 3 na - 1
-        nb       = min 2 nb
-        play :: [DieValue] -> [DieValue] -> (Int, Int)
-        play a d = let f  = sortBy (flip compare)
-                       zp = zipWith (compare) (f a) (f d)
-                       n  = length . filter (== GT) $ zp
-                   in (length zp - n, n)
+  attack <- replicateM na die
+  defend <- replicateM nd die
+  let (da, dd) = play attack defend
+  return $ Battlefield (a-da) (d-dd)
+  where na = min 3 a - 1
+        nd = min 2 d
+
+invade :: Battlefield -> Rand StdGen Battlefield
+invade b@(Battlefield a d)
+  | d == 0 || a < 2 = return b
+  | otherwise       = battle b >>= invade
+
+isAttackWin :: Battlefield -> Rand StdGen Bool
+isAttackWin initial = do
+  (Battlefield a d) <- invade initial
+  return (a >= 2)
+
+successProb :: Battlefield -> Rand StdGen Double
+successProb initial = do
+  res <- replicateM 100000 (isAttackWin initial)
+  return . calc $ res
+  where calc r = let win = length . filter id $ r
+                 in fromIntegral win / 100000.0
+
+exactSuccessProb :: Battlefield -> Double
+exactSuccessProb (Battlefield a d) = prob ! (a,d)
+  where prob   :: Array (Int, Int) Double
+        prob   = array ((0,0),(a,d)) [ ((i,j),go i j) | i <- [0..a],
+                                                        j <- [0..d] ]
+        go a d
+          | a < 2     = 0
+          | d == 0    = 1
+          | d < 2     = (prob ! (a, d-1) + prob ! (a-1, d)) / 2.0
+          | otherwise = sum [ prob ! (a-1,d-1),
+                              prob ! (a-2,d),
+                              prob ! (a,d-2) ] / 3.0
