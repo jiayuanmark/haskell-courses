c05c77503f528bca9e17759fdf4ab53a87549fdc
jiayuanmark <jiayuanmark@gmail.com>
2016-01-13 23:06:54 -0800

    buggy hw12

--- cis194/hw12/Risk.hs
+++ cis194/hw12/Risk.hs
@@ -5,7 +5,7 @@ module Risk where
 import Control.Monad
 import Control.Monad.Random
 import Data.Array
-import Data.List (sortBy)
+import Data.List (sortBy, splitAt)
 
 ------------------------------------------------------------
 -- Die values
@@ -31,10 +31,10 @@ type Army = Int
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
 
 play :: [DieValue] -> [DieValue] -> (Int, Int)
-play attackDie defendDie = (length zp - n, n)
-  where f  = sortBy (flip compare)
-        zp = zipWith (compare) (f attackDie) (f defendDie)
-        n  = length . filter (== GT) $ zp
+play attackDie defendDie = (length cmp - awn, awn)
+  where dsc  = sortBy (flip compare)
+        cmp  = zipWith (>) (dsc attackDie) (dsc defendDie)
+        awn  = length $ filter id cmp
 
 battle :: Battlefield -> Rand StdGen Battlefield
 battle (Battlefield a d) = do
@@ -50,17 +50,36 @@ invade b@(Battlefield a d)
   | d == 0 || a < 2 = return b
   | otherwise       = battle b >>= invade
 
-isAttackWin :: Battlefield -> Rand StdGen Bool
-isAttackWin initial = do
-  (Battlefield a d) <- invade initial
-  return (a >= 2)
-
 successProb :: Battlefield -> Rand StdGen Double
-successProb initial = do
-  res <- replicateM 100000 (isAttackWin initial)
-  return . calc $ res
-  where calc r = let win = length . filter id $ r
-                 in fromIntegral win / 100000.0
+successProb b = do
+  res <- replicateM 100000 (invade b)
+  return $ (fl . filter attackWin $ res) / (fl res)
+  where attackWin (Battlefield _ d) = d == 0
+        fl = fromIntegral . length
+
+-- Exact probability calculation
+data Result = Attacker | Defender | Tie
+  deriving (Show, Eq)
+
+prb :: [Double]
+prb = let a  = length $ filter (== Attacker) sim
+          d  = length $ filter (== Defender) sim
+          t  = length $ filter (== Tie) sim
+      in map (\x -> (fromIntegral x) / (6.0^5)) [a, d, t]
+  where go :: [Int] -> Result
+        go x
+          | a1 >  d1 && a2 >  d2 = Attacker
+          | a1 <= d1 && a2 <= d2 = Defender
+          | otherwise            = Tie
+          where (as, ds)  = splitAt 3 x
+                (a1:a2:_) = sortBy (flip compare) as
+                (d1:d2:_) = sortBy (flip compare) ds
+        sim = do a1 <- [1..6]
+                 a2 <- [1..6]
+                 a3 <- [1..6]
+                 d1 <- [1..6]
+                 d2 <- [1..6]
+                 return $ go [a1, a2, a3, d1, d2]
 
 exactSuccessProb :: Battlefield -> Double
 exactSuccessProb (Battlefield a d) = prob ! (a,d)
@@ -75,11 +94,10 @@ exactSuccessProb (Battlefield a d) = prob ! (a,d)
                             dwin = (sum . map (\x -> (x / 6) ^ a') $ [1..6]) / 6
                         in (1 - dwin) * prob ! (a, d-1) + dwin * prob ! (a-1, d)
           -- only 1 attacker die
-          | a == 2    = let awin =
-                        in
+          | a == 2    = let awin = (sum . map (\x -> (x / 6) ^ 2) $ [1..5]) / 6
+                        in awin * prob ! (a, d-1) + (1 - awin) * prob ! (a-1, d)
           -- 3 attacker die and 2 defender die
-          | a >= 4    =
+          | a >= 4    = sum $ zipWith (*) prb sub
           -- 2 attacker die and 2 defender die
-          | otherwise = sum [ prob ! (a-1,d-1),
-                              prob ! (a-2,d),
-                              prob ! (a,d-2) ] / 3.0
+          | otherwise = (sum sub) / 3.0
+          where sub = [ prob ! (a,d-2), prob ! (a-2,d), prob ! (a-1,d-1) ]
